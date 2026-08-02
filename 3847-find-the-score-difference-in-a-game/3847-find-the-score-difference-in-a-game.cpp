62dc3c7a9d4ed3d4265865b7c19f21f06329d509
Rishi Bhuva <rish690712@gmail.com>
2026-08-02 17:23:45 +0530

    Time: 0 ms (100%), Space: 40.9 MB (40.91%) - LeetHub

---
+++ 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int scoreDifference(vector<int>& nums) {
+        int dif=0;
+        bool act=true;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]%2!=0)
+            act=!act;
+            if((i+1)%6==0)
+            act=!act;
+            if(act)
+            dif+=nums[i];
+            else
+            dif-=nums[i];
+        }
+        return dif;
+    }
+};

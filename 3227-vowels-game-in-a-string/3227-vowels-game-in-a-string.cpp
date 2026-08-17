4cc3f69211a211085fa71b663912fe74f3f6e5e2
Rishi Bhuva <rish690712@gmail.com>
2026-08-17 21:42:32 +0530

    Time: 8 ms (42.99%), Space: 18.9 MB (95.42%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
-        int c=0;
         for(int i=0;i<s.size();i++)
         {
             if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
-            c++;
+            return true;
         }
-        if(c==0)
         return false;
-        else 
-        return true;
     }
 };

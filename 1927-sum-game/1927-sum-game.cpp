dc942d30af037fe5ec6f29758b4b23e511117051
Rishi Bhuva <rish690712@gmail.com>
2026-08-23 09:10:53 +0530

    Time: 4 ms (47.55%), Space: 14 MB (33.57%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n=num.size();
+        int ls=0,rs=0,lq=0,rq=0;
+        for(int i=0;i<n/2;i++)
+        {
+            if (num[i]=='?') 
+            lq++;
+            else 
+            ls+=num[i]-'0';
+        }
+        for(int i=n/2;i<n;i++)
+        {
+            if(num[i]=='?')
+            rq++;
+            else
+            rs+=num[i]-'0';
+        }
+        if((lq+rq)%2==1 || (ls-rs)!=9*(rq-lq)/2) 
+        return true;
+        return false;
+    }
+};

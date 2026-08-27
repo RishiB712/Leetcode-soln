33053bc6f6feea3e17501600cc20024b3646b90d
Rishi Bhuva <rish690712@gmail.com>
2026-08-27 22:55:55 +0530

    Time: 0 ms (100%), Space: 8.2 MB (43.48%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        stack<int> st;
+        int ans=0;
+        for(char c:s)
+        {
+            if(c=='(')
+            {
+                st.push(ans);
+                ans=0;
+            }
+            else
+            {
+                ans=st.top()+max(2*ans,1);
+                st.pop();
+            }
+        }
+        return ans;
+    }
+};

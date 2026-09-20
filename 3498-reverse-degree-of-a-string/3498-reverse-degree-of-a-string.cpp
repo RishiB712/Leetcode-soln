b06b093cec844d6bc071875ac7c145239a0f2bf6
Rishi Bhuva <rish690712@gmail.com>
2026-09-20 09:10:42 +0530

    Time: 0 ms (100%), Space: 9.6 MB (78.4%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int n=s.size(),ans=0;
+        for(int i=0;i<n;i++)
+        ans+=((26-s[i]+'a')*(i+1));
+        return ans;
+    }
+};
